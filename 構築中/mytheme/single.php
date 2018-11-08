f464af0c61654f368eea20db49edb206755d93dc
Ryo Suzuki <ryo.private@gmail.com>
2018-11-08 21:50:01 +0900

    yoru

--- 構築中/mytheme/single.php
+++ 構築中/mytheme/single.php
@@ -1,4 +1,20 @@
 <?php get_header(); ?>
+<div class="sub-header">
+    <div class="bread">
+        <ol>
+            <li><a href="<?php echo home_url(); ?>"><span class="fas fa-home ic-home"></span><span class="link-top">TOP</span></a></li>
+            <li>
+                <?php if(has_category()): ?>   
+                    <?php $postcat = get_the_category();
+                    echo get_category_parents($postcat[0], true, '</li><li>');
+                    endif;
+                    ?>
+                <a><?php the_title();?></a>
+            </li>
+        </ol>
+    </div>
+</div>
+
 <div class="container">
     <div class="contents">
     <?php if(have_posts()):while(have_posts()):the_post();?>
@@ -31,6 +47,17 @@
             <?php endif; ?>
 
             <?php the_content(); ?>
+
+<div class="share">
+
+<ul>
+    <li><a href="" class="share_tw"><i class="fab fa-twitter"></i>twitterでシェア</a></li>
+    <li><a href="" class="share_fb"><i class="fab fa-facebook"></i>facebookでシェア</a></li>
+    <li><a href="" class="share_gp"><i class="fab fa-google-plus-square"></i>Google+ でシェア</a></li>
+</ul>
+
+</div>
+
         </article>
     <?php endwhile; endif; ?>
     </div>
